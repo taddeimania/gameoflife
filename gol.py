107299621d1e7b82915f416d66365487ddbe2784
Joel Taddei <jtaddei@gmail.com>
2012-04-27 23:48:51 -0500

    init

---
+++ gol.py
@@ -0,0 +1,104 @@
+import random
+import copy
+
+class LifeNode():
+
+    def __init__(self):
+        rand = random.random()
+        if rand < 0.5:
+            self.lifestate = 1
+        else:
+            self.lifestate = 0
+
+    def __str__(self):
+        states = {0:" ", 1:"X"}
+        return states[self.lifestate]
+
+class CreateBoard():
+
+    def __init__(self, size):
+        self.BOARD = []
+        self.NEW_BOARD = []
+        self.size = size
+
+    def generate_board(self):
+        row = []
+        for x in range(self.size):
+            for y in range(self.size):
+               row.append(LifeNode())
+            self.BOARD.append(row)
+            row = []
+
+    def draw_board(self, board):
+        for row in board:
+            for cell in row:
+                print "", cell, "",
+            print "\n"
+
+    def iterate_board(self):
+        tmp = self.copy_board(self.BOARD)
+        for idx, row in enumerate(self.BOARD):
+            for idxx, cell in enumerate(row):
+                locale_tuple = (idx, idxx)
+                self.check_neighbors(tmp, locale_tuple)
+        self.BOARD = self.copy_board(tmp)
+        self.draw_board(self.BOARD)
+
+    def copy_board(self, board):
+        return copy.deepcopy(board)
+
+    def check_neighbors(self, tmp, locale_tuple):
+        count = self.count_neighbors(locale_tuple)
+        if count < 2:
+            self.die_cell(tmp, locale_tuple)
+        if count in [2,3] and tmp[locale_tuple[0]][locale_tuple[1]].lifestate == 1:
+            self.live_cell(tmp, locale_tuple)
+        if count == 3:
+            self.live_cell(tmp, locale_tuple)
+        if count > 3:
+            self.die_cell(tmp, locale_tuple)
+
+    def count_neighbors(self, loc):
+        neighbor_count = 0
+        high_bound = self.size - 1
+        if loc[0] > 0:
+            if loc[1] > 0:
+                neighbor_count += self.BOARD[loc[0] - 1][loc[1] - 1].lifestate
+            neighbor_count += self.BOARD[loc[0] - 1][loc[1]].lifestate
+            if loc[1] < high_bound:
+                neighbor_count += self.BOARD[loc[0] - 1][loc[1] + 1].lifestate
+
+        if loc[1] > 0:
+            neighbor_count += self.BOARD[loc[0]][loc[1] - 1].lifestate
+        if loc[1] < high_bound:
+            neighbor_count += self.BOARD[loc[0]][loc[1] + 1].lifestate
+
+        if loc[0] < high_bound:
+            if loc[1] > 0:
+                neighbor_count += self.BOARD[loc[0] + 1][loc[1] - 1].lifestate
+            neighbor_count += self.BOARD[loc[0] + 1][loc[1]].lifestate
+            if loc[1] < high_bound:
+                neighbor_count += self.BOARD[loc[0] + 1][loc[1] + 1].lifestate
+        return neighbor_count
+
+    def die_cell(self, board, loc):
+        board[loc[0]][loc[1]].lifestate = 0
+
+    def live_cell(self, board, loc):
+        board[loc[0]][loc[1]].lifestate = 1
+
+
+def main():
+    import time
+    import os
+    board = CreateBoard(15)
+    board.generate_board()
+    board.draw_board(board.BOARD)
+    while True:
+        #dummy = raw_input()
+        os.system('clear')
+        board.iterate_board()
+        time.sleep(.05)
+
+if __name__ == "__main__":
+    main()
---
+++ gol_tests.py
@@ -0,0 +1,53 @@
+import unittest
+import gol
+from gol import LifeNode
+import mock
+
+class LifeNodeTest(unittest.TestCase):
+
+    def test_init_can_make_alive_or_dead_node(self):
+        lifestates = [0,1]
+        node = gol.LifeNode()
+        self.assertIn(node.lifestate, lifestates)
+
+    def test_life_node_returns_str_representation(self):
+        node = gol.LifeNode()
+        node.lifestate = 0
+        self.assertEqual(" ", str(node))
+
+class CreateBoardTest(unittest.TestCase):
+
+    def setUp(self):
+        self.board = gol.CreateBoard(3)
+        self.copy_patch = mock.patch('copy.deepcopy')
+        self.copy = self.copy_patch.start()
+
+    def tearDown(self):
+        self.copy_patch.stop()
+
+    def test_generate_board_fills_board_with_life_nodes(self):
+        self.board.generate_board()
+        for idx, row in enumerate(self.board.BOARD):
+            for idxx, cell in enumerate(row):
+                self.assertIsInstance(self.board.BOARD[idx][idxx], LifeNode)
+
+    @mock.patch('gol.CreateBoard.copy_board')
+    def test_iterate_board_calls_copy_board_twice(self, copy_board):
+        new_board = gol.CreateBoard(3)
+        new_board.iterate_board()
+        self.assertEqual(2, copy_board.call_count)
+
+    @mock.patch('gol.CreateBoard.check_neighbors')
+    def test_iterate_board_calls_check_neighbors(self, check_neighbors):
+        
+
+    @unittest.skip(True)
+    @mock.patch('gol.CreateBoard.count_neighbors')
+    def test_check_neighbors_calls_count_neighbors(self, count_neighbors):
+        new_board = gol.CreateBoard(3)
+        new_board.check_neighbors()
+
+    def test_copy_board_calls_deep_copy(self):
+        new_board = gol.CreateBoard(3)
+        newer_board = new_board.copy_board(self.board.BOARD)
+        self.copy.assert_called_once_with(self.board.BOARD)
